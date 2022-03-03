dc3b741845d726985932b6b967d0bb48b3a147c3
P-PremSai <71306858+P-PremSai@users.noreply.github.com>
2022-03-03 14:44:12 +0530

    Delete lib directory

--- lib/models/salarys.dart
+++
@@ -1,9 +0,0 @@
-class salarys {
-  final String Basic_DA;
-  final String HRA;
-  final String Bonus;
-  final String Other;
-  final String total;
-
-  salarys({this.Basic_DA, this.HRA, this.Bonus, this.Other, this.total});
-}
